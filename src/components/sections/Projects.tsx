4571d09e13fbd7624a659eeddc945d06e24d0b15
noeltesaluna <ntesaluna03@gmail.com>
2025-02-27 10:55:18 +1100

    aligned projects to left for mobile

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -10,18 +10,19 @@ function Projects() {
     <Flex 
       w={'100dvw'}   
       justify={'flex-start'}
-      align={'center'}
+      alignItems={{base: 'flex-start', md: 'center'}}
       direction={'column'}
+      p={'1em'}
       py={20}
       >
       <Heading size='4xl' color={'cyan.500'} fontWeight={'bold'} pb={'0.5em'}>PROJECTS</Heading>
     <Flex 
       justify={'flex-start'}
-      align={'center'}
+      alignItems={{base: 'flex-start', md: 'center'}}
       direction={'column'}
       gap={10}
       
-      p={'1em'}
+      
     >
         
         {projects.map((project) => <ProjectCard {...project}
